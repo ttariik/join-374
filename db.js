bde1e06e04f3bb440251cacd818c7450e3475bc1
Ioannis Karakasidis <168634993+Ioannis-Karakasidis@users.noreply.github.com>
2024-10-10 14:16:23 +0200

    Update db.js

--- db.js
+++ db.js
@@ -3,11 +3,22 @@ const GLOBAL =
 
 function onLoadFunc() {
   console.log("test");
-  loadData();
+  postData("/name", { name: "rama" });
 }
 
 async function loadData() {
   let response = await fetch(GLOBAL + ".json");
   let responseToJson = await response.json();
   console.log(responseToJson);
+}
+
+async function postData(path = "", data = {}) {
+  let response = await fetch(GLOBAL + ".json", {
+    method: "POST",
+    header: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  return (responseToJson = response.json());
 }
