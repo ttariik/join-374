62d87cf6aa44f57c70497760453d05b9b80070e6
Ioannis Karakasidis <168634993+Ioannis-Karakasidis@users.noreply.github.com>
2024-10-10 13:58:04 +0200

    .

--- db.js
+++ db.js
@@ -1,15 +1,13 @@
-
+let GLOBAL =
+  "https://join-backend-dd268-default-rtdb.europe-west1.firebasedatabase.app/";
 
 function onLoadFunc() {
-    console.log('test');
-    loadData("")
+  console.log("test");
+  loadData();
 }
 
-const GLOBAL = "https://join-backend-dd268-default-rtdb.europe-west1.firebasedatabase.app/";
-
-async function loadData(path ="") {
-    let response = await fetch (BASE_URL + path + ".json");
-    let responseToJson = await response.json();
-    console.log(responseToJson);
-    
+async function loadData() {
+  let response = await fetch(GLOBAL + ".json");
+  let responseToJson = await response.json();
+  console.log(responseToJson);
 }
