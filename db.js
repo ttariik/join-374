98b60cbfd5bd93b409d71e9918b4fe3ae818bf8a
Ioannis Karakasidis <168634993+Ioannis-Karakasidis@users.noreply.github.com>
2024-10-11 12:14:01 +0200

    firebase profiles

--- db.js
+++ db.js
@@ -1,23 +1,58 @@
+let users = [];
 const GLOBAL =
   "https://join-backend-dd268-default-rtdb.europe-west1.firebasedatabase.app/";
 
-function onLoadFunc() {
-  postData("O8qLwXTcpuwmcg1hMwW/", { name: "page" });
+async function onLoadFunc() {
+  let userResponse = await getAllUsers("users");
+
+  let UserKeyArray = Object.keys(userResponse);
+  for (let index = 0; index < UserKeyArray.length; index++) {
+    users.push({
+      id: UserKeyArray[index],
+      user: userResponse[UserKeyArray[index]],
+    });
+  }
+  await addEditSingleUser((id = 1), {
+    name: "Ioannis",
+    lastname: "Karakasidis",
+    username: "Ioannis",
+    password: "123",
+    contacts: {
+      name: "peter",
+      telefone: "123456789",
+    },
+    tasks: {
+      name: "Log in / Sign up Logik",
+    },
+    inprogress: {
+      name: "Backend",
+    },
+    awaitingfeedback: {
+      name: "Backend",
+    },
+    done: {
+      name: "board drag and drop",
+    },
+  });
+  console.log(users);
 }
 
-async function loadData() {
-  let response = await fetch(GLOBAL + ".json");
-  let responseToJson = await response.json();
-  console.log(responseToJson);
-}
-
-async function postData(path = "", data = {}) {
-  let response = await fetch(GLOBAL + ".json", {
-    method: "POST",
-    header: {
+async function putData(path = "", data = {}) {
+  let response = await fetch(GLOBAL + path + ".json", {
+    method: "PUT",
+    headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(data),
   });
-  return (responseToJson = response.json());
+  return (responsetoJson = await response.json());
+}
+
+async function addEditSingleUser(id = 1, user = { name: "Kevin" }) {
+  putData(`users/${id}`, user);
+}
+
+async function getAllUsers(path) {
+  let response = await fetch(GLOBAL + path + ".json");
+  return (responsetoJson = await response.json());
 }
