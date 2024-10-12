a3b93b5d51d20b036c1a8859cd21a93776c9201e
Ioannis Karakasidis <168634993+Ioannis-Karakasidis@users.noreply.github.com>
2024-10-12 23:18:39 +0200

    .

---
+++ scripts/add-contact.js
@@ -0,0 +1,53 @@
+let users = [];
+const GLOBAL =
+  "https://join-backend-dd268-default-rtdb.europe-west1.firebasedatabase.app/";
+
+async function addcontact() {
+  let userResponse = await getAllUsers("users");
+  let telefonename = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
+  let UserKeyArray = Object.keys(userResponse);
+  for (let index = 0; index < UserKeyArray.length; index++) {
+    users.push({
+      id: UserKeyArray[index],
+      user: userResponse[UserKeyArray[index]],
+    });
+  }
+  await addEditSingleUser((id = 1), {
+    contacts: [
+      {
+        name: telefonename,
+        email: email,
+        telefone: phone,
+      },
+    ],
+  });
+  emptyinputs();
+}
+
+function emptyinputs() {
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("phone").value = "";
+}
+
+async function putData(path = "", data = {}) {
+  let response = await fetch(GLOBAL + path + ".json", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  return (responsetoJson = await response.json());
+}
+
+async function addEditSingleUser(id = 1, contact = { name: "Kevin" }) {
+  putData(`users/${id}`, contact);
+}
+
+async function getAllUsers(path) {
+  let response = await fetch(GLOBAL + path + ".json");
+  return (responsetoJson = await response.json());
+}
